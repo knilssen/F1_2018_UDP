f7adeabbddea3b3c06c0da6abf3fbe9d94ab0ad8
Kristian Nilssen <kcn0519@westminstercollege.edu>
2018-09-07 00:11:52 +1000

    Fixed Issue
    Fixed the issue we had. Now grabs the first 21 bits and sends to header struct. Using the header we can find out which packet we are reciving, then work with it accordingly.

--- f1_python/f1_udp_client.py
+++ f1_python/f1_udp_client.py
@@ -3,7 +3,10 @@ import ctypes
 import sys
 import binascii
 import structs
+from pprint import pprint
 
+packet_structures = [structs.PacketMotionData, structs.PacketSessionData, structs.PacketLapData, structs.PacketEventData, structs.PacketParticipantsData, structs.PacketCarSetupData, structs.PacketCarTelemetryData, structs.PacketCarStatusData]
+packet_names = ['MotionData', 'SessionData', 'LapData', 'ParticipantData', 'CarSetupData', 'CarTelemetryData', 'PacketCarStatusData']
 
 def get_packet(address, port):
     """
@@ -22,22 +25,18 @@ def get_packet(address, port):
     # recieve data
     data, addr = sock.recvfrom(1341)
 
-    # convert from raw bytes to UDPPacket structure
-    pack_len = len(data)
-    print pack_len
 
-    # return data
-    # If packet recieved is Motion Data
-    if pack_len == 1341:
-        print "Motion Data Recieved"
-        return structs.PacketMotionData.from_buffer_copy(data)
-    # If packet recieved is Seesion Data
-    elif pack_len == 147:
-        print "Session Data Recieved"
-        return structs.PacketSessionData.from_buffer_copy(data)
-    # If packet recieved is Lap Data
-    # elif pakc_len == 841:
-    #     return structs.PacketSessionData.from_buffer_copy(data)
+    # Read the packet header and determine which UDPPacket is being recieved from the game
+    header_data = data[0:21]
+    packet_header = structs.PacketHeader.from_buffer_copy(header_data)
+    packet_id = packet_header.m_packetId
+
+    # Print out the packets name
+    print packet_names[packet_id]
+
+    # return data and convert from raw bytes to UDPPacket structure
+    return packet_structures[packet_id].from_buffer_copy(data)
+
 
 
 
@@ -51,17 +50,17 @@ def get_telemetry(address, port):
 
     while True:
         packet = get_packet(address, port)
-    # if last_packet is None or packet.time > last_packet.time:
-        yield packet
-        last_packet = packet
+        if last_packet is None or packet.m_header.m_sessionTime > last_packet.m_header.m_sessionTime:
+            yield packet
+            last_packet = packet
 
 
 def main(address, port):
     # Enter the ip and port you are using
-    for packet in get_telemetry('127.0.0.1', 5003):
-        print packet
+    for packet in get_telemetry(address, port):
+        pprint(dir(packet))
 
-
+        
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         # Kristians Usuage:     python python_udp_test.py  127.0.0.1 5003
